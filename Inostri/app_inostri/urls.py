35b14ac02da3c03a813a545a584e759b045e190c
Alejo Poch <alejopoch@live.com.ar>
2023-05-06 17:11:05 -0300

    No encuentra los html

--- Inostri/app_inostri/urls.py
+++ Inostri/app_inostri/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from app_inostri.views import listar_vinos, listar_bodegas, listar_varietales, crear_vino, buscar_vino
 
 urlpatterns = [
-    path('vinos/', listar_vinos, name='listar_vinos'),
+    path('vinos/', listar_vinos, name='lista_vinos'),
     path('crear-vinos/', crear_vino, name='crear_vino'),
     path('buscar-vino/', buscar_vino, name='buscar_vino'),
     path('bodegas/', listar_bodegas, name='lista_bodegas'),
